53d4b759bd62989187a918e2b7d9a1c3bcfee158
mitinmi <slootlite@yandex.ru>
2019-07-21 18:41:55 +0700

    Добавил бд

---
+++ WordCheat/SQLiteDB.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SQLite;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WordCheat
+{
+    class SQLiteDB
+    {
+        private static string dbFileName = "db.sqlite";
+        private static SQLiteConnection m_dbConn;
+        private static SQLiteCommand m_sqlCmd;
+        public static void Connect()
+        {
+            m_dbConn = new SQLiteConnection("Data Source=" + dbFileName + ";Version=3;");
+            m_dbConn.Open();
+            m_sqlCmd.Connection = m_dbConn;
+        }
+
+        public static void Disconnect()
+        {
+            m_dbConn.Close();
+        }
+
+        public static void ExecQuery(string sql)
+        {
+            m_sqlCmd.CommandText = sql;
+            m_sqlCmd.ExecuteNonQuery();
+        }
+
+        public static DataTable Select(string sql)
+        {
+            DataTable dTable = new DataTable();
+            SQLiteDataAdapter adapter = new SQLiteDataAdapter(sql, m_dbConn);
+            adapter.Fill(dTable);
+            return dTable;
+        }
+    }
+}
